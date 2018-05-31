a433d42f8d7ad163a362620f9666d7d4eab033d3
Emmanuel Pregnolato <emmanuel_pregnolato@manulife.com>
2018-05-31 19:44:00 -0400

    Initial

---
+++ src/main/java/pluralsight/demo/RateController.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package pluralsight.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author emmanuelpregnolato
+ *
+ */
+@Controller
+public class RateController {
+
+	@Value("${rate}")
+	String rate;
+	
+	@Value("${lanecount}")
+	String lanecount;
+	
+	@Value("${tollstart}")
+	String tollstart;
+	
+	@RequestMapping("/rate")
+	public String getRate(Model model) {
+		
+		model.addAttribute("rateamount", rate);
+		model.addAttribute("lanes", lanecount);
+		model.addAttribute("tollstart", tollstart);
+		
+		return "rateview";
+	}
+}
